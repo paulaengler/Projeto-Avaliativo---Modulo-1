adaf7cd50b6cbfa4c85609c79f05537750d9df96
paulaengler <paulaengler@live.com>
2024-05-02 10:13:23 -0300

    Criação models

---
+++ src/models/Local.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require("sequelize");
+const { connection } = require("../database/connection");
+
+const Local = connection.define('locais', {
+    nome_do_local: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    descrição:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    localidade:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    coordenadas_geograficas:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+})
+
+//coordenadas? qual tipo??
